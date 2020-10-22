72f23806f5e81c0c74ef55ded401765feaae5ec9
Bhupesh Vyas <Bhupesh.Vyas@msg-global.com>
2020-10-22 15:42:00 +0530

    Initial commit

---
+++ vehical-data-analysis-server/src/main/java/vehicaldataanalysisserver/vehicaldataanalysisserver/VehicalDataAnalysisServerApplication.java
@@ -0,0 +1,13 @@
+package vehicaldataanalysisserver.vehicaldataanalysisserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicalDataAnalysisServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VehicalDataAnalysisServerApplication.class, args);
+    }
+
+}
