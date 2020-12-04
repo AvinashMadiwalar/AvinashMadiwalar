72f1ffb5b55fffcf21a0b0e739b9d4b8f7b57047
Bhupesh Vyas <Bhupesh.Vyas@msg-global.com>
2020-12-04 18:19:11 +0530

    added security

--- vehical-data-analysis-server/src/main/java/vehicaldataanalysisserver/vehicaldataanalysisserver/VehicalDataAnalysisServerApplication.java
+++ vehical-data-analysis-server/src/main/java/vehicaldataanalysisserver/vehicaldataanalysisserver/VehicalDataAnalysisServerApplication.java
@@ -4,8 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class , SecurityAutoConfiguration.class})
+@EnableResourceServer
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class VehicalDataAnalysisServerApplication {
 
     public static void main(String[] args) {
